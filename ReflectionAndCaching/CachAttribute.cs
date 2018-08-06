0adfd8da278d8d3374c5ac38a52938771f7e7343
Anastasiya Galtsova <galtsova98@gmail.com>
2018-08-06 14:30:37 +0300

    Add a ReflectionAndCaching workshop homework task.

---
+++ ReflectionAndCaching/CachAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ReflectionAndCaching
+{
+    public class CachAttribute : Attribute
+    {
+        public CachAttribute()
+        {
+        }
+
+        public CachAttribute(int hour, int min, int sec)
+        {
+            CachingTime = new TimeSpan(hour, min, sec);
+        }
+
+        public TimeSpan CachingTime { get; set; }
+    }
+}
